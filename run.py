c17c36491f5d2657bf769d140237deba1cf079c7
Freddy2296 <Garciafreddy328@yahoo.com>
2021-09-03 20:01:08 +0000

    modified run.py to display search results

--- run.py
+++ run.py
@@ -21,3 +21,18 @@ def search_page():
 
 if __name__ == '__main__':
   app.run()
+
+
+@app.route('/recipes')
+def get_recipes():
+  if (str(request.args['ingridients']).strip() != ""):
+      # If there is a list of ingridients -> list
+      querystring = {"number":"5","ranking":"1","ignorePantry":"false","ingredients":request.args['ingridients']}
+      response = requests.request("GET", url + find, headers=headers, params=querystring).json()
+      return render_template('recipes.html', recipes=response)
+  else:
+      # Random recipes
+      querystring = {"number":"5"}
+      response = requests.request("GET", url + randomFind, headers=headers, params=querystring).json()
+      print(response)
+      return render_template('recipes.html', recipes=response['recipes'])
